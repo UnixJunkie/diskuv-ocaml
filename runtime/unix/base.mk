0846dc953e55901b05495906ac063f813fb9a9cf
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2021-08-25 01:44:24 -0700

    Prerelease v0.1.0-prerel4

--- runtime/unix/base.mk
+++ runtime/unix/base.mk
@@ -1,7 +1,7 @@
 #######################################
 # base.mk
 
-DKML_BASE_VERSION = 0.1.0-prerel3
+DKML_BASE_VERSION = 0.1.0-prerel4
 
 ifdef COMSPEC
 DKML_NONEMPTY_IF_BUILD_HOST_IS_WINDOWS = ON
